456d9dd189dc068816c31a4aa1d40bcc15340bc4
faridisayev <faridisayev3@icloud.com>
2023-08-31 17:48:40 +0300

    Add mergeSort.cpp

---
+++ mergeSort.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+
+using namespace std;
+
+void merge(int arr[], int l, int m, int r)
+{
+    int i = l;
+
+    int j = m + 1;
+
+    int k = l;
+
+    int temp[5];
+
+    while (i <= m && j <= r)
+    {
+        if (arr[i] <= arr[j])
+        {
+            temp[k] = arr[i];
+
+            i ++;
+
+            k ++;
+        }
+        else
+        {
+            temp[k] = arr[j];
+
+            j ++;
+
+            k ++;
+        }
+    }
+
+    while (i <= m)
+    {
+        temp[k] = arr[i];
+
+        i ++;
+
+        k ++;
+    }    
+
+    while (j <= r)
+    {
+        temp[k] = arr[j];
+
+        j ++;
+
+        k ++;
+    }
+
+    for (int p = l; p <= r; p ++)
+    {
+        arr[p] = temp[p];
+    }
+}
+
+void mergeSort(int arr[], int l, int r)
+{
+    if (l < r)
+    {
+        int m = (l + r) / 2;
+
+        mergeSort(arr, l, m);
+        
+        mergeSort(arr, m + 1, r);
+
+        merge(arr, l, m, r);
+    }
+}
+
+int main()
+{
+    int arr[5] = {4, 6, 1, 2, 7};
+
+    cout << "Unsorted: ";
+
+    for (int i = 0; i < 5; i ++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    cout << endl;
+
+    mergeSort(arr, 0, 4);
+
+    cout << "Sorted: ";
+
+    for (int i = 0; i < 5; i ++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    cout << endl;
+
+    return 0;
+}
